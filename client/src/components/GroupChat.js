611dcefeaecb6ffe0de3948c2701a1dc0b833c7a
ilkedogan <ilke.dogan@ug.bilkent.edu.tr>
2022-12-05 17:29:29 +0300

    group chat

---
+++ client/src/components/GroupChat.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import Colors from "../utils/Color";
+import MessageCard from "./MessageCard";
+import { Grid } from "@mui/material";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100%",
+    width: "100%",
+    background: Colors.primary_dark,
+    display: "flex",
+    color: "white",
+    borderTop: "1px solid #819CA9",
+  },
+});
+
+const MessageBar = () => {
+  const classes = useStyles();
+
+  // mock data for now
+  const [name, photo] = useState([
+    {
+      name: "Murat Furkan Uğurlu",
+      ppUrl: "https://www.google.com",
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: false,
+      nonReadCount: 4,
+      ppUrl: null,
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: true,
+      nonReadCount: 0,
+      ppUrl: null,
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: false,
+      nonReadCount: 0,
+      ppUrl: null,
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: true,
+      nonReadCount: 0,
+      ppUrl: "https://www.google.com",
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: false,
+      nonReadCount: 0,
+      ppUrl: null,
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: true,
+      nonReadCount: 1,
+      ppUrl: null,
+    },
+    {
+      name: "Murat Furkan Uğurlu",
+      message: "Hello World!",
+      time: "12:00",
+      isIncoming: false,
+      nonReadCount: 3,
+      ppUrl: null,
+    },
+  ]);
+
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          <div style={{ height: "85vh", overflowY: "scroll" }}>
+            {messages.map((message) => (
+              <MessageCard
+                name={message.name}
+                message={message.message}
+                time={message.time}
+                isIncoming={message.isIncoming}
+                nonReadCount={message.nonReadCount}
+                ppUrl={message.ppUrl}
+              />
+            ))}
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default MessageBar;
--- client/src/components/UserBar.js
+++ client/src/components/UserBar.js
@@ -7,6 +7,18 @@ import PeopleIcon from '@mui/icons-material/People';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+const handleGroupChat = () => {
+
+};
+
+const handleSendingPrivateMessage = () => {
+
+};
+
+const handeSettings = () => {
+
+};
+
 const useStyles = makeStyles({
   root: {
     height: "8%",
@@ -25,16 +37,21 @@ const UserBar = () => {
     <div className={classes.root}>
       <Grid container>
         <Grid item xs={6}>
-          <AccountCircleIcon style={{fontSize: "45px", marginLeft: "10px"}}/>
+          <AccountCircleIcon style={{ fontSize: "45px", marginLeft: "10px" }} />
         </Grid>
         <Grid item xs={2}>
-          <PeopleIcon style={{fontSize: "25px", marginTop: "10px", marginLeft: "50px"}}/>
+          <PeopleIcon
+            style={{ fontSize: "25px", marginTop: "10px", marginLeft: "50px", cursor:"pointer" }}
+            onClick={() => handleGroupChat()} />
+
         </Grid>
         <Grid item xs={2}>
-          <ChatIcon style={{fontSize: "25px", marginTop: "10px", marginLeft: "50px"}}/>
+          <ChatIcon style={{ fontSize: "25px", marginTop: "10px", marginLeft: "50px" , cursor:"pointer" }}
+            onClick={() => handleSendingPrivateMessage()} />
         </Grid>
         <Grid item xs={2}>
-          <MoreVertIcon style={{fontSize: "25px", marginTop: "10px", marginLeft: "30px"}}/>
+          <MoreVertIcon style={{ fontSize: "25px", marginTop: "10px", marginLeft: "30px", cursor:"pointer" }}
+            onClick={() => handleSettings()} />
         </Grid>
       </Grid>
     </div>
